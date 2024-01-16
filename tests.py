36655c5042b312053d7363dd8bbd499781466dab
Philip-Daniel Ebsworth <84278185+Phil-Ebsworth@users.noreply.github.com>
2024-01-17 00:21:45 +0100

    deleted tests

--- tests.py
+++
@@ -1,8 +0,0 @@
-from color import rgb_split
-from utils import load_image
-
-
-def test_rgb_split():
-    img = load_image(path_to_image='Images/Flower.png')
-    splits = rgb_split(img)
-    assert (splits) == rgb_split(img)
